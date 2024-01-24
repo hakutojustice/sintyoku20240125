5ccb62a8fc6338f2437d94b18eef9148fff338c4
kakisawa <uk9im3@outlook.jp>
2024-01-24 22:28:36 +0900

    up

--- 2347059_Kawasaki/PalutenaGame/Scene/SceneGameOver.cpp
+++ 2347059_Kawasaki/PalutenaGame/Scene/SceneGameOver.cpp
@@ -1,5 +1,6 @@
 #include "SceneGameOver.h"
 #include "SceneManager.h"
+#include "SceneMain.h"
 #include "Pad.h"
 #include "Game.h"
 #include "DxLib.h"
@@ -8,24 +9,30 @@ namespace
 {
 	// 文字の表示位置
 	constexpr int kChirPosX = kScreenWidth * 0.4;
-	constexpr int kChirPosY = kScreenHeight * 0.7;
+	constexpr int kChirPosY = kScreenHeight * 0.3;
+
+	// 文字の表示幅
+	constexpr int kCharInterval = 120;
 
 	// 文字を囲む四角の初期位置
 	constexpr int kSelectPosX = kChirPosX - 2;
-	constexpr int kSelectPosY = kChirPosY - 6;
+	constexpr int kSelectPosY = kChirPosY - 2;
 
 	// 文字を囲む四角の移動量
-	constexpr int kSelectMoveY = 35;
+	constexpr int kSelectMoveY = 120;
 
 	// 文字を囲む四角のサイズ
-	constexpr int kSelectSizeX = 120;
-	constexpr int kSelectSizeY = 30;
+	constexpr int kSelectSizeX = 700;
+	constexpr int kSelectSizeY = 75;
 }
 
 SceneGameOver::SceneGameOver() :
 	m_isSceneEnd(false),
+	isStage1(false),
+	isStage2(false),
 	m_select(kScelectRestart),
-	m_fadeAlpha(255)
+	m_fadeAlpha(255),
+	m_selectPos(kSelectPosX, kSelectPosY)
 {
 
 }
@@ -40,7 +47,11 @@ void SceneGameOver::Init()
 
 	m_select=kScelectRestart;
 	m_isSceneEnd = false;
+	isStage1 = false;
+	isStage2 = false;
 	m_fadeAlpha = 255;
+	m_selectPos.x = kSelectPosX;
+	m_selectPos.y = kSelectPosY;
 }
 
 void SceneGameOver::Update()
@@ -73,9 +84,21 @@ void SceneGameOver::Update()
 	// エンターキーが押されたらタイトル画面へ遷移する
 	if (Pad::IsTrigger(PAD_INPUT_4))
 	{
+		SceneMain* pSceneMain = new SceneMain;
+
 		switch (m_select)
 		{
 		case kScelectRestart:
+			if(pSceneMain->JustFinishStage1())
+			{
+				m_isSceneEnd = true;
+				isStage1 = true;
+			}
+			//if(m_pSceneTwo->JustStage2())
+			//{
+			// m_isSceneEnd = true;
+			// isStage2=true;
+			//}
 			break;
 		case kScelectReturnHome:
 			m_isSceneEnd = true;
@@ -118,12 +141,22 @@ void SceneGameOver::Draw()
 {
 	DrawGraph(0, 0, Graph, false);
 	DrawString(120, 120, "ゲームオーバー画面", GetColor(255, 255, 255));
-	DrawString(120, 120 + 16, "Aキーを押してください", GetColor(255, 255, 255));
+	DrawString(120, 120 + 64, "Aキーを押してください", GetColor(255, 255, 255));
 
 	// フェードの描画
 	SetDrawBlendMode(DX_BLENDMODE_ALPHA, m_fadeAlpha);	// 半透明で表示開始
 	DrawBox(0, 0, kScreenWidth, kScreenHeight, GetColor(255, 255, 255), true);
 	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);		// 不透明に戻しておく
+
+	SetFontSize(64);
+
+	DrawString(kChirPosX, kChirPosY, "もう一度最初から遊ぶ", 0xffffff);
+	DrawString(kChirPosX, kChirPosY + kCharInterval, "タイトル画面に戻る", 0xffffff);
+	DrawString(kChirPosX, kChirPosY + kCharInterval * 2, "ゲームを終わる", 0xffffff);
+
+	// 選択中の部分を四角で描画
+	DrawBox(m_selectPos.x, m_selectPos.y, m_selectPos.x + kSelectSizeX,
+		m_selectPos.y + kSelectSizeY, 0x00bfff, false);
 }
 
 void SceneGameOver::End()
@@ -135,9 +168,4 @@ void SceneGameOver::End()
 bool SceneGameOver::IsSceneEnd() const
 {
 	return m_isSceneEnd;
-}
-
-bool SceneGameOver::IsSceneRestart() const
-{
-	return false;
 }
